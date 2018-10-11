6e19aec24404c3ad760143c93bcbfa5a5f6e9472
BitPrivate-Pro <43960301+BitPrivate-Pro@users.noreply.github.com>
2018-10-12 02:14:27 +0800

    Update ChangeLanguageDialog.cpp

--- src/gui/ChangeLanguageDialog.cpp
+++ src/gui/ChangeLanguageDialog.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2016 The Karbowanec developers
-// Copyright (c) 2018 The Brazukcoin developers
+// Copyright (c) 2018 The bitprivate developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
